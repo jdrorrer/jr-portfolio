43d96796e1b553ba4aad9bd7c14f5eff3b9f38f2
Jake Rorrer <jdrorrer@gmail.com>
2014-10-11 10:13:20 -0400

    Added styling and jQuery for active links in navigation menu

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+// Show active link on navigation
+$('.inline-list a').click(function(){
+   //remove class from prior active link
+   $('.selected').removeClass('selected');
+   //"this" is current link clicked
+   $(this).addClass('selected');
+});
