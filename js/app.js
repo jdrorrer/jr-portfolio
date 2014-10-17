1c954e2718662c67ce0da8c8973114aa85c39341
Jake Rorrer <jdrorrer@gmail.com>
2014-10-17 07:08:24 -0400

    Added lightbox for images.

--- js/app.js
+++ js/app.js
@@ -3,17 +3,17 @@
 $(document).foundation();
 
 // Show active link on navigation
-$('.main-nav ul li a').click(function() {
-	//Remove class from prior active link
-	$('.selected').removeClass('selected');
-	//"this" is current link clicked
-	$(this).addClass('selected');
-});
+// $('.main-nav ul li a').click(function() {
+// 	//Remove class from prior active link
+// 	$('.selected').removeClass('selected');
+// 	//"this" is current link clicked
+// 	$(this).addClass('selected');
+// });
 
 // Show Home as active link when logo clicked
 $('.logo a').click(function() {
    	$('.selected').removeClass('selected');
-   	$('.main-nav ul li a[href="#home-section"]').addClass('selected');
+   	$('.main-nav ul li a[data-scroll="home-section"]').addClass('selected');
 });
 
 
@@ -39,3 +39,90 @@ $('a.hamburger').click(function() {
 		});
 	}
 });
+
+//Scroll animation to proper section when a link in main header is clicked
+$('.main-nav ul li a').on('click', function() {
+	var scrollAnchor = $(this).attr('data-scroll'),
+    	scrollPoint = $('h1[data-anchor="' + scrollAnchor + '"]').offset().top;
+
+    $('body,html').animate({
+        scrollTop: scrollPoint
+    }, 500);
+
+    return false;
+});
+
+//Scroll animation to home section when logo image is clicked
+$('.logo a').click(function() {
+	var scrollAnchor = $(this).attr('data-scroll'),
+		scrollPoint = $('h1[data-anchor="home-section"]').offset().top; 
+
+	$('body,html').animate({
+        scrollTop: scrollPoint
+    }, 500);
+
+    return false;
+});
+
+//Change active link based on scroll position
+$(window).scroll(function() {
+    var windscroll = $(window).scrollTop();
+    if (windscroll >= 138) {
+        $('h1[id$="section"]').each(function(i) {
+        	var offset = $(this).offset();
+        	// console.log(offset, offset.top, windscroll+1);
+            if (offset.top <= windscroll + 1) {
+                $('.selected').removeClass('selected');
+                $('.main-nav ul li a').eq(i).addClass('selected');
+            }
+        });
+    } else {
+        $('.selected').removeClass('selected');
+        $('.main-nav ul li a:first').addClass('selected');
+    }
+
+}).scroll();
+
+//Shrink main header when scrolling down
+$(window).scroll(function() {
+	var windscroll = $(window).scrollTop();
+	if (windscroll >= 138) {
+		$('.main-header').addClass('shrink-header');
+	} else {
+		$('.main-header').removeClass('shrink-header');
+	}
+}).scroll();
+
+// //Code to instantiate imageLightBox plugin
+// $(function() {
+// 	$('a').imageLightbox();
+// });
+
+// //Code to add style elements to imageLightBox plugin
+// var imgAltText = $('.work-img').attr('alt')
+// var $overlay = $('<div id="imagelightbox-overlay"></div>');
+// var $closeButton = $('<button type="button" id="imagelightbox-close" title="Close"></button>');
+// var $leftArrow = $('<button type="button" class="imagelightbox-arrow imagelightbox-arrow-left" style="display: block;"></button>');
+// var $rightArrow = $('<button type="button" class="imagelightbox-arrow imagelightbox-arrow-right" style="display: block;"></button>');
+// var $caption = $('<div id="imagelightbox-caption">' + imgAltText + '</div>');
+
+// $overlay.append([$closeButton, $leftArrow, $rightArrow, $caption]);
+
+// $('.work-img').click(function() {
+// 	$('body').append($overlay);
+// });
+
+// $overlay.click(function() {
+// 	$overlay.remove();
+// });
+
+$('.img-box').magnificPopup({
+  delegate: 'a', // child items selector, by clicking on it popup will open
+  type: 'image',
+  // other options
+  gallery:{enabled:true}
+});
+
+
+
+
